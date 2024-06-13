0c3a7274edee913aa750e6219754821ea571b517
flashgun <flashgun@xboxstat.ru>
2024-06-13 15:12:39 +0000

    fixed: forgot to add getlangs to project :)

---
+++ faststat/getlangs.c
@@ -0,0 +1,109 @@
+#include <stdlib.h>
+#include <libpq-fe.h>
+
+#include "stater.h"
+
+void setlang(uint64 xuid, int cid, int lid)	{
+
+	int p;
+
+	for(p = xuids->root; p != -1;)
+		if(((XUID *)ftree_get(xuids, p))->xuid == xuid)
+			break;
+		else if(((XUID *)ftree_get(xuids, p))->xuid >= xuid)
+			p = ftree_get(xuids, p)->l;
+		else
+			p = ftree_get(xuids, p)->r;
+
+	if( p == -1 )		// no xuid
+		return;
+
+	((XUID *)ftree_get(xuids, p))->xuid |= (uint64)cid << 48;
+	((XUID *)ftree_get(xuids, p))->xuid |= (uint64)lid << 56;
+
+}
+
+void readlangs()	{
+
+	int r;
+	
+	union llichar ll;
+	union intchar i;
+
+	uint64 xuid = 0;
+	int cid = 0, lid = 0;
+
+	char *buf;
+
+	printf("Reading country/language info\n");
+
+	r = PQsendQueryParams(conn, "select xuid,countryid,langid from gamers where countryid is not null",
+			0,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			1);			// binary format
+
+	if( ! r )	{
+
+		printf("Error '%s' reading langs/countries tab\n", PQerrorMessage(conn));
+		exit(1);
+
+	}
+
+	printf("Setting SingleRowMode\n");
+
+	r = PQsetSingleRowMode(conn);
+
+	if ( ! r )	{
+
+		printf("Error '%s' setting SingleRowMode\n", PQerrorMessage(conn));
+		exit(1);
+
+	}
+
+	printf("SingleRowMode is set\n");
+
+	while( (res = PQgetResult(conn)) != NULL)	{
+
+		ExecStatusType rs;
+
+		rs = PQresultStatus(res);
+		if( rs != PGRES_SINGLE_TUPLE)	{
+			PQclear(res);
+			continue;
+		}
+
+		buf = PQgetvalue(res, 0, 0);		// xuid
+		ll.c[0] = buf[7];
+		ll.c[1] = buf[6];
+		ll.c[2] = buf[5];
+		ll.c[3] = buf[4];
+		ll.c[4] = buf[3];
+		ll.c[5] = buf[2];
+		// ll.c[6] = buf[1];				// 0x09
+		// ll.c[7] = buf[0];				// 0x00
+		xuid = ll.i;
+
+		buf = PQgetvalue(res, 0, 1);		// countryid
+		i.c[0] = buf[1];
+		i.c[1] = buf[0];
+		cid = i.i;
+
+		buf = PQgetvalue(res, 0, 2);		// langid
+		i.c[0] = buf[1];
+		i.c[1] = buf[0];
+		lid = i.i;
+
+		setlang(xuid, cid, lid);
+
+		PQclear(res);
+
+	}
+
+	printf("Well done\n");
+
+}
+
+
